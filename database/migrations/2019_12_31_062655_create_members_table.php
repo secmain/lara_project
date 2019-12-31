2649a97e5b361040220c3c493f8b42dae94d7acd
root <root@localhost.localdomain>
2019-12-31 16:33:30 +0900

    create membersTable migration file

---
+++ database/migrations/2019_12_31_062655_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 200);
+            $table->string('email', 200);
+            $table->string('camera', 200)->nullable();
+            $table->integer('camera_reki')->nullable();
+            $table->timestamps();
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('members');
+    }
+}
