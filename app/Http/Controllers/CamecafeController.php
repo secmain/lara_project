d0dad4f0fc81110cdcd44c0888140130f230eb74
root <root@localhost.localdomain>
2019-12-31 22:29:01 +0900

    contactのバリデーションチェック処理

--- app/Http/Controllers/CamecafeController.php
+++ app/Http/Controllers/CamecafeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\ContactFormRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Mail\Message;
 
 class CamecafeController extends Controller
 {
@@ -36,10 +38,16 @@ class CamecafeController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function postContact(Request $request)
+    public function postContact(ContactFormRequest $request)
     {
+        /*
         //
-        return 'hello !!!';
+        \Mail::send('emails.hello', ['name' => 'tanaka'], function (Message $message) {
+            $message->to('circlecle4586@gmail.com', '太郎')
+                    ->subject('お知らせ');
+        });
+        */
+        return view('camecafe.contact_end');
     }
 
     /**
---
+++ app/Http/Requests/ContactFormRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class ContactFormRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'name' => ['required', 'max:50'],
+            'email' => ['required', 'email'],
+            'camera' => ['max:200'],
+            'camera_reki' => ['digits:3'],
+            'text' => ['required', 'max:1000']
+        ];
+    }
+}
